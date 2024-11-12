f0b9121b83907d403f3fa6dd380bb8ba23bae68a
TI95 <t.ilvess95@gmail.com>
2024-11-12 20:31:33 +0200

    dasboard styles changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -64,7 +64,7 @@ const Page = async () => {
 
     return (
         <div className='flex min-h-screen w-full bg-muted/40'>
-            <div className='max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4'>
+            <div className='max-w-7xl w-full mx-4 flex flex-col sm:gap-4 sm:py-4 my-4'>
                 <div className='flex flex-col gap-16'>
                     <div className='grid gap-4 sm:grid-cols-2'>
                         <Card>
